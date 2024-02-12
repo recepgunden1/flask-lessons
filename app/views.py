0112c68698ac5b534a320366d489b20e148cbfc5
recep <recepgunden35@gmail.com>
2024-02-12 19:05:20 +0300

    cookie oluşturuldu

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def definition():
-    return "<html><body><h1>ilk Flask denemesi</h1></body></html>"
+    response = make_response("<html><body><h1>ilk Flask denemesi</h1></body></html>")
+    response.set_cookie('name', 'Ahmet')
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
